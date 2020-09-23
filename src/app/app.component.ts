f5ac6fef27eec63935f779d13db6be8b62c78f83
Nimit Jain <nimit.jain@vlinkinfo.com>
2020-09-23 18:20:34 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,37 @@
 import { Component } from '@angular/core';
-
+import { FormGroup, FormControl, Validators} from '@angular/forms';
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+   selector: 'app-root',
+   templateUrl: './app.component.html',
+   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my-app';
+   title = 'Angular 6 Project!';
+   todaydate;
+   componentproperty;
+   name;
+   date;
+   emailid;
+   passwd;
+   address;
+   mobile;
+   city;
+   formdata;
+   ngOnInit() {
+      this.formdata = new FormGroup({
+         emailid: new FormControl("", Validators.compose([
+            Validators.required,
+            Validators.pattern("[^ @]*@[^ @]*")
+         ])),
+         name: new FormControl(""),
+         date: new FormControl(""),
+         passwd: new FormControl(""),
+         address: new FormControl(""),
+         mobile: new FormControl(""),
+         city: new FormControl(""),
+         
+      });
+   }
+   onClickSubmit(data) {this.emailid = data.emailid; this.passwd = data.passwd; this.address = data.address; this.mobile = data.mobile; this.city = data.city; this.name = data.name; this.date = data.date;}
+  
 }
